e55d6bd289599483a7317d719845dc7ccacc4def
lucas-docha <lucas_d_borges@estudante.sesisenai.org.br>
2025-06-06 12:53:19 -0300

    sincronização geral - login e pagina principal

---
+++ tela login/TelaLogin.js
@@ -0,0 +1,7 @@
+ function googlelogin() {
+            window.open("https://accounts.google.com/v3/signin/identifier?continue=https%3A%2F%2Fwww.google.com%3Fhl%3Dpt-PT&ec=GAlA8wE&hl=pt-PT&flowName=GlifWebSignIn&flowEntry=AddSession&dsh=S1678227823%3A1749223930685292", "_blank");
+        }
+
+function facebooklogin() {
+            window.open("https://pt-br.facebook.com/login/device-based/regular/login/", "_blank");
+        }
