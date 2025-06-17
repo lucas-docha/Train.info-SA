8e5bf66f29bd625ffe27482691f4e53e91e47c8b
lucas-docha <lucas_d_borges@estudante.sesisenai.org.br>
2025-06-16 21:46:52 -0300

    pre-finalização tela principal

--- TelaRotas/dadosrotas.js
+++ TelaRotas/dadosrotas.js
@@ -1,32 +1,33 @@
 window.onload = function () {
 
 
-    //dados rota1 
+    // Rota 1
     const nidR1 = 289;
     const mR1 = "carvão";
     const sR1 = "em andamento";
     const nR1 = 77;
 
-    document.getElementById("infoid1").innerHTML = `ID da Rota: ${nidR1}`
-    document.getElementById("info1").innerHTML = `Material: ${mR1}, Status:  ${sR1}, Rota: ${nR1}`
+    document.getElementById("infoid1").innerHTML = `${nidR1}`;
+    document.getElementById("info1").innerHTML = `${mR1} - ${sR1} - ${nR1}`;
 
-    //dados rota2
+    // Rota 2
     const nidR2 = 989;
     const mR2 = "cascalho";
     const sR2 = "finalizada";
     const nR2 = 90;
 
-    document.getElementById("infoid2").innerHTML = ` ID da Rota: ${nidR2}`
-    document.getElementById("info2").innerHTML = `Material: ${mR2}, Status: ${sR2}, Rota: ${nR2}`
+    document.getElementById("infoid2").innerHTML = `${nidR2}`;
+    document.getElementById("info2").innerHTML = `${mR2} - ${sR2} - ${nR2}`;
 
-    //dados rota3
+    // Rota 3
     const nidR3 = 1893;
     const mR3 = "arroz";
     const sR3 = "finalizada";
     const nR3 = 567;
 
-    document.getElementById("infoid3").innerHTML = ` ID da Rota: ${nidR3}`
-    document.getElementById("info3").innerHTML = `Material: ${mR3}, Status: ${sR3}, Rota: ${nR3}`
+    document.getElementById("infoid3").innerHTML = `${nidR3}`;
+    document.getElementById("info3").innerHTML = `${mR3} - ${sR3} - ${nR3}`;
+
 }
 
 //ver mapa
@@ -35,42 +36,22 @@ function vernomapa() {
 }
 
 
-//rotas matuÊ
-const niveis = [1, 2, 3, 4];
-const nivel = 1;
+//validação de cor de status da rota
 
-const idStatus = [
-    { id: 'statusTremRota', nivel: 2 },
-    { id: 'statusTremRota2', nivel: 1 },
-    { id: 'statusTremRota3', nivel: 3 },
-    { id: 'statusTremRota4', nivel: 4 },
-    { id: 'statusTremRota5', nivel: 2 },
-    { id: 'statusTremRota6', nivel: 1 },
-    { id: 'statusTremRota7', nivel: 3 },
-    { id: 'statusTremRota8', nivel: 4 },
-     { id: 'statusTremRota8', nivel: 4 },
-];
+const elementos = document.querySelectorAll('.status-geral-cor');
 
-idStatus.forEach(item => {
-    const texto = document.getElementById(item.id);
+elementos.forEach(p => {
+    const texto = p.textContent.toLowerCase();
 
-    if (!texto) return;
-
-    switch (item.nivel) {
-        case 1:
-            texto.style.color ="green";
-            break;
-        case 2:
-            texto.style.color ="yellow";
-            break;
-        case 3:
-            texto.style.color ="lime green #ADFF2F";
-            break;
-        case 4:
-            texto.style.color ="red";
-            break;
+    if (texto.includes('bom')) {
+        p.style.color = '#c1ff72';
+    } else if (texto.includes('ótimo')) {
+        p.style.color = 'green';
+    } else if (texto.includes('médio')) {
+        p.style.color = '#f8bc29';
+    } else if (texto.includes('ruim')) {
+        p.style.color = 'red';
     }
-
-})
+});
 
 
